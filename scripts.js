d2f8449c25022f83477295319199bebb35cfeba9
Michael Hilt <michael_hilt@comcast.com>
2022-07-21 13:10:16 -0400

    Completed 23.8.2 up to 2.d

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.onload = function() {
+  
+  const takeoff = document.getElementById("takeoff");
+  const flightStatus = document.getElementById("flightStatus");
+  const shuttleBackground = document.getElementById("shuttleBackground");
+  const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+  
+  takeoff.addEventListener("click", function(event) {
+    let response = window.confirm("Confirm that the shuffle is ready for takeoff.");
+      if(response) {
+        flightStatus.innerHTML = "Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = "blue";
+        spaceShuttleHeight.innerHTML += "10000";
+      }
+  })
+
+};
